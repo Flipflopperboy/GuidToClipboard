fe709c61ac697a1333c29f7fedb819f616335942
Jesper <jesper.o.astrom@gmail.com>
2012-01-26 10:05:55 +0100

    First commit

---
+++ Source/Flip.Tools.GuidToClipboard/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+
+namespace Flip.Tools.GuidToClipboard
+{
+
+	internal class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			Clipboard.SetText(Guid.NewGuid().ToString().ToLower());
+		}
+
+	}
+
+}
---
+++ Source/Flip.Tools.GuidToClipboard/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿[assembly: System.Reflection.AssemblyTitle("GuidToClipboard")]
+[assembly: System.Reflection.AssemblyCompany("")]
+[assembly: System.Reflection.AssemblyProduct("GuidToClipboard")]
+[assembly: System.Reflection.AssemblyCopyright("Jesper Åström")]
+
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("e1b14279-740d-4246-b679-8704d9b51311")]
+
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
